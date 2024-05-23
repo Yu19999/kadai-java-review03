3fc3473c6e06c5abc1c8b9b0908e5a471b30ada9
yu <yu912912@gmail.com>
2024-05-23 21:49:33 +0900

    1回目

---
+++ src/baseball/BaseBallTeam.java
@@ -0,0 +1,44 @@
+package baseball;
+
+public class BaseBallTeam {
+        private String name;
+        private int win;
+        private int lose;
+        private int draw;
+
+        public BaseBallTeam(String name, int win, int lose, int draw) {
+            this.name = name;
+            this.win = win;
+            this.lose = lose;
+            this.draw = draw;
+        }
+
+        public double getRate() {
+            return (double) win / (win + lose);
+        }
+
+        public void report() {
+            System.out.println(Baseballteam + "の2022年の成績は" + win"勝" + lose"敗" + draw"分"、"勝率は" + getrate　+ "です”);
+
+        }
+
+        // Getter
+        public String getName() {
+            return name;
+        }
+
+        public int getWin() {
+            return win;
+        }
+
+        public int getLose() {
+            return lose;
+        }
+
+        public int getDraw() {
+            return draw;
+        }
+    }
+
+
+}
